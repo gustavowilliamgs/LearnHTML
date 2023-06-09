f894d8cc401c72b66c64f1df9246f382536beb64
Gustavo William <gustavowilliamgs1@gmail.com>
2023-06-08 23:00:41 -0300

    Deixando a primeira pagina responsiva
    Co-Authored-By: Us3rmanu <134158821+Us3rmanu@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const btnVerCursoEl = document.querySelector('#btn-ver-curso');
+const menuEl = document.querySelector('#menu');
+const submenuEl = document.querySelector('#submenu');
 
-btnVerCursoEl.addEventListener('click', function () {
+btnVerCursoEl.addEventListener('click', () => {
     window.location.href = '#btn-fazer-curso';
+});
+
+menuEl.addEventListener('click', () => {
+    submenuEl.classList.toggle('ativo');
 });
