b6ade57fc74aaaa2992360273e4070265acf97bd
Gustavo William <gustavowilliamgs1@gmail.com>
2023-06-12 21:07:03 -0300

    Atualizai o easter egg e retirei as imagens

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const btnVerCursoEl = document.querySelector('#btn-ver-curso');
 const btnFazerCursoEl = document.querySelector('#btn-fazer-curso');
+const tituloEl = document.title;
+
+window.addEventListener('blur', () => {
+    document.title = "Learn CSS"
+});
+
+window.addEventListener('focus', () => {
+    document.title = tituloEl;
+});
 
 btnVerCursoEl.addEventListener('click', () => {
     window.location.href = '#btn-fazer-curso';
