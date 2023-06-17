0d4f1482fb9f5e16431f5ea74988f4aa3b61d4bf
Gustavo William <gustavowilliamgs1@gmail.com>
2023-06-17 17:03:25 -0300

    easter-egg concluído
    Co-Authored-By: Us3rmanu <134158821+Us3rmanu@users.noreply.github.com>

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,8 @@ const submenuEl = document.querySelector('#submenu');
 
 menuEl.addEventListener('click', () => {
     submenuEl.classList.toggle('ativo');
+    if (submenuEl.className == 'ativo')
+        document.documentElement.style.overflow = 'hidden';
+    else
+        document.documentElement.style.overflow = 'auto';
 });
