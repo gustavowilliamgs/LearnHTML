9c260be264a6a759792631c44d97ef3817925de7
Gustavo William <gustavowilliamgs1@gmail.com>
2023-06-17 15:35:10 -0300

    Muitas atualizações foram feitas
    Hoje, terminamos as páginas das aulas, todas elas foram feitas,  iniciamos o easter-egg, organizamos os arquivos em pastas e criamos o favicon.
    
    Co-Authored-By: playerstar69 <109179654+playerstar69@users.noreply.github.com>
    Co-Authored-By: Us3rmanu <134158821+Us3rmanu@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 const btnVerCursoEl = document.querySelector('#btn-ver-curso');
 const btnFazerCursoEl = document.querySelector('#btn-fazer-curso');
+const iconEl = document.head.querySelector(`link[rel="shortcut icon"]`);
 const tituloEl = document.title;
 
 window.addEventListener('blur', () => {
-    document.title = "Learn CSS"
+    document.title = "LearnCSS";
+    iconEl.href = "./img/favicon-css.ico";
 });
 
 window.addEventListener('focus', () => {
     document.title = tituloEl;
+    iconEl.href = "./img/favicon.ico";
 });
+
 
 btnVerCursoEl.addEventListener('click', () => {
     window.location.href = '#btn-fazer-curso';
 });
 
 btnFazerCursoEl.addEventListener('click', () => {
-    window.location.href = './aulas/aula-1.html';
+    window.location.href = './html/aula-1.html';
 });
